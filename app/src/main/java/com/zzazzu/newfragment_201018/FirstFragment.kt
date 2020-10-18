cb9a8e81ebbd141687e4ab85ce603015bdf61743
bucephalus41 <bucephalus41@gmail.com>
2020-10-19 01:52:31 +0900

    Initial commit

---
+++ app/src/main/java/com/zzazzu/newfragment_201018/FirstFragment.kt
@@ -0,0 +1,29 @@
+package com.zzazzu.newfragment_201018
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeTextBtn.setOnclickListener {
+
+            first.text = "버튼이 눌림"
+        }
+    }
+}
